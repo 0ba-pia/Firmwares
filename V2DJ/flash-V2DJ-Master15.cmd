7f3d8b705061c903d438d83bdb88a8ad3c14f26d
pianodevoyage <contact@pianodevoyage.com>
2024-06-30 17:10:33 +0200

    windows

---
+++ V2DJ/flash-V2DJ-Master15.cmd
@@ -0,0 +1,2 @@
+echo "Flashing device on port $1."
+C:\Users\obalbous\AppData\Local\Arduino15\packages\rp2040\tools\pqt-python3\1.0.1-base-3a57aed/python3 -I C:\Users\obalbous\AppData\Local\Arduino15\packages\rp2040\hardware\rp2040\2.2.2/tools/uf2conv.py --serial $1 --family RP2040 --deploy MasterRP2040-V2DJ15_2.9.7.0.ino.uf2
