f1cdf2c26b78d824b84bbac08ce88f1e77dcb5a5
pianodevoyage <contact@pianodevoyage.com>
2024-07-01 12:13:29 +0200

    PATH PR2040

--- V2DJ/flash-V2DJ-VK25.cmd
+++ V2DJ/flash-V2DJ-VK25.cmd
@@ -1,4 +1,2 @@
 echo "Flashing device on port $1."
-C:\Users\obalbous\AppData\Local\Arduino15\packages\rp2040\tools\pqt-python3\1.0.1-base-3a57aed/python3 -I C:\Users\obalbous\AppData\Local\Arduino15\packages\rp2040\hardware\rp2040\2.2.2/tools/uf2conv.py --serial $1 --family RP2040 --deploy V2DVK25-RP2040.ino.generic.uf2
-
-
+python3 -I "%RP2040%\uf2conv.py" --serial $1 --family RP2040 --deploy V2DVK25-RP2040.ino.generic.uf2
