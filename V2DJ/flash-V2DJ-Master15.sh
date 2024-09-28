42bc60a2ff7ff1a26b3b8a687fdf58939c97b3eb
0ba-pia <40423058+0ba-pia@users.noreply.github.com>
2024-09-28 17:11:20 +0200

    Path review for Mac

--- V2DJ/flash-V2DJ-Master15.sh
+++ V2DJ/flash-V2DJ-Master15.sh
@@ -1,6 +1,6 @@
 echo "Flashing device /dev/cu.usbmodem$1."
-/Users/obalbous/Library/Arduino15/packages/rp2040/tools/pqt-python3/1.0.1-base-3a57aed/python3 \
--I /Users/obalbous/Library/Arduino15/packages/rp2040/hardware/rp2040/3.1.1/tools/uf2conv.py \
+python3 \
+-I ~/Library/Arduino15/packages/rp2040/hardware/rp2040/4.0.3/tools/uf2conv.py \
 --serial /dev/cu.usbmodem$1 \
 --family RP2040 \
 --deploy MasterRP2040-V2DJ15_2.9.7.0.ino.uf2 
