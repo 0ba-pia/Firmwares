e684d6e09a79393185b4a5b68f8146b624669423
0ba-pia <40423058+0ba-pia@users.noreply.github.com>
2025-01-29 16:29:07 +0100

    2.9.7.2

--- V2DJ/flash-V2DJ-Master24.sh
+++ V2DJ/flash-V2DJ-Master24.sh
@@ -3,4 +3,4 @@ python3 \
 -I ~/Library/Arduino15/packages/rp2040/hardware/rp2040/4.0.3/tools/uf2conv.py \
 --serial /dev/cu.usbmodem$1 \
 --family RP2040 \
---deploy MasterRP2040-V2DJ24_2.9.7.0.ino.uf2 
+--deploy MasterRP2040-V2DJ24_2.9.7.2.ino.uf2 
