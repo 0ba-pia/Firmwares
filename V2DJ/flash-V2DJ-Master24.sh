1649a9ac4e86cbe54c70603f1de070b22fb1d03b
0m1 <40423058+0ba-pia@users.noreply.github.com>
2025-04-14 16:58:50 +0200

    Reverted to 2.9.7.0 version

--- V2DJ/flash-V2DJ-Master24.sh
+++ V2DJ/flash-V2DJ-Master24.sh
@@ -1,6 +1,8 @@
-echo "Flashing device /dev/cu.usbmodem$1."
+echo "***** Flashing device /dev/cu.usbmodem$1."
+FW="MasterRP2040-V2DJ24_2.9.7.0.ino.uf2"
 python3 \
 -I ~/Library/Arduino15/packages/rp2040/hardware/rp2040/4.0.3/tools/uf2conv.py \
 --serial /dev/cu.usbmodem$1 \
 --family RP2040 \
---deploy MasterRP2040-V2DJ24_2.9.7.2.ino.uf2 
+--deploy $FW
+echo "***** DONE with $FW"
