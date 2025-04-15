46550fb9293e26e729bf1100d1ff53e501f18dc2
0m1 <40423058+0ba-pia@users.noreply.github.com>
2025-04-15 15:57:11 +0200

    Mac shortcut friendly

--- V2DJ/flash-V2DJ-Master24.sh
+++ V2DJ/flash-V2DJ-Master24.sh
@@ -1,8 +1,10 @@
-echo "***** Flashing device /dev/cu.usbmodem$1."
-FW="MasterRP2040-V2DJ24_2.9.7.0.ino.uf2"
+echo "**** Flashing device /dev/cu.usbmodem$1."
+rp2040=4.0.3
+FW=~/Documents/GitHub/Firmwares/V2DJ/MasterRP2040-V2DJ24_2.9.7.0.ino.uf2
+echo "**** Firmware : $FW"
 python3 \
--I ~/Library/Arduino15/packages/rp2040/hardware/rp2040/4.0.3/tools/uf2conv.py \
+-I ~/Library/Arduino15/packages/rp2040/hardware/rp2040/$rp2040/tools/uf2conv.py \
 --serial /dev/cu.usbmodem$1 \
 --family RP2040 \
 --deploy $FW
-echo "***** DONE with $FW"
+
--- V2DJ/flash-V2DJ-VK24.sh
+++ V2DJ/flash-V2DJ-VK24.sh
@@ -1,6 +1,9 @@
-echo "Flashing device /dev/cu.usbmodem$1."
+echo "**** Flashing device /dev/cu.usbmodem$1."
+rp2040=4.0.3
+FW=~/Documents/GitHub/Firmwares/V2DJ/V2DJ/V2DVK24-RP2040.ino.generic.uf2
+echo "**** Firmware : $FW"
 python3 \
--I ~/Library/Arduino15/packages/rp2040/hardware/rp2040/4.0.3/tools/uf2conv.py \
+-I ~/Library/Arduino15/packages/rp2040/hardware/rp2040/$rp2040/tools/uf2conv.py \
 --serial /dev/cu.usbmodem$1 \
 --family RP2040 \
---deploy V2DVK24-RP2040.ino.generic.uf2
+--deploy $FW
