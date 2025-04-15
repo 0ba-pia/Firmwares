4008a5466699a568176c776e460cdb60c9242252
0m1 <40423058+0ba-pia@users.noreply.github.com>
2025-04-15 18:10:26 +0200

    Paths

--- V2DJ/flash-V2DJ-VK24.sh
+++ V2DJ/flash-V2DJ-VK24.sh
@@ -1,6 +1,6 @@
 echo "**** Flashing device /dev/cu.usbmodem$1."
 rp2040=4.0.3
-FW=~/Documents/GitHub/Firmwares/V2DJ/V2DJ/V2DVK24-RP2040.ino.generic.uf2
+FW=~/Documents/GitHub/Firmwares/V2DJ/V2DVK24-RP2040.ino.generic.uf2
 echo "**** Firmware : $FW"
 python3 \
 -I ~/Library/Arduino15/packages/rp2040/hardware/rp2040/$rp2040/tools/uf2conv.py \
